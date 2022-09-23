953fecfcefb756709ebcca263d5bdadaa9098f24
luc <luckdina45@gmail.com>
2022-09-23 18:23:00 +0100

    save chenge x11

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -100,19 +100,23 @@ const patient = async (req ,res ,next) => {
             let sql = `select profileId_fk from qr_code where id = "${req.params.id}"`;
             let [responsedb, _] = await db.query(sql);
 
-            console.log('profile id ::' , responsedb[0].profileId_fk);
+            if (responsedb[0]) {
+                console.log('profile id ::' , responsedb[0]);
 
-            data = await Profile.byId(responsedb[0].profileId_fk);
-            console.log(data);
-            if(utils.empty(data)) {
-                return res.status(403).json({err:"ce indentifiant n'est pas valide"});
-            }
-            else {
                 data = await Profile.byId(responsedb[0].profileId_fk);
-                return res.status(200).json({
-                    id: data[0].userId_fk,
-                    data: DataShape.patientInfo(data)
-                });
+                console.log(data);
+                if(utils.empty(data)) {
+                    return res.status(403).json({err:"ce indentifiant n'est pas valide"});
+                }
+                else {
+                    data = await Profile.byId(responsedb[0].profileId_fk);
+                    return res.status(200).json({
+                        id: data[0].userId_fk,
+                        data: DataShape.patientInfo(data)
+                    });
+                }
+            } else {
+                res.status(500).json({err:"identifiant incorecte"});
             }
         }
 
